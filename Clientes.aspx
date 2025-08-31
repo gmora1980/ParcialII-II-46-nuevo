f0b059b0fb44166ec38cd8d40cd38e35bf18d38f
gusmorji <165494334+gusmorji@users.noreply.github.com>
2025-08-30 21:39:59 -0600

    Añadir página Clientes y actualizar lógica de login
    Se ha creado la nueva página ASP.NET `Clientes.aspx` con su código subyacente y archivo de diseñador. Se han eliminado las clases de encriptación en `Simple3Des.vb`, indicando que ya no se utilizará este mecanismo. También se han actualizado las referencias en el proyecto para incluir los nuevos archivos. Además, se ha modificado la lógica de redirección en `login.aspx.vb` para redirigir a "Clientes.aspx" y se ha añadido una función para verificar las credenciales del cliente.

---
+++ Clientes.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Clientes.aspx.vb" Inherits="ParcialII_II_46_nuevo.Clientes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
--- login.aspx.vb
+++ login.aspx.vb
@@ -1,12 +1,29 @@
-﻿Public Class login
+﻿Imports System.Data.SqlClient
+
+Public Class login
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Session("ClienteId") Is Nothing Then
-            Response.Redirect("default.aspx")
-        End If)
+            Response.Redirect("Clientes.aspx")
+        End If
     End Sub
+    Protected Function verificarcliente(Clienteingresado As Cliente) As Cliente
+        Try
+            Dim db As New DatabaseHelper()
+            Dim parameters As New List(Of SqlParameter) From {
+                New SqlParameter("@Email", Clienteingresado.Email),
+                New SqlParameter("@Password", Clienteingresado.Clave)
+            }
+            Dim consulta As String = "SELECT * FROM Clientes WHERE Email = @Email AND Password = @Password"
+            Dim dt As DataTable = db.ExecuteQuery(consulta, parameters)
+            If dt.Rows.Count = 0 Then
 
+            End If
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
     Protected Sub btnLogin_Click(sender As Object, e As EventArgs)
 
     End Sub
